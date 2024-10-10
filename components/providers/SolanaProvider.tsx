38bdd5a60c814be67996dd8c436f19822b57e57e
leopham <hongthaipro@gmail.com>
2024-10-10 15:46:36 +0700

    ad moongate

--- components/providers/SolanaProvider.tsx
+++ components/providers/SolanaProvider.tsx
@@ -7,10 +7,28 @@ import {
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import { clusterApiUrl } from "@solana/web3.js";
+import { registerMoonGateWallet } from "@moongate/moongate-adapter";
+import { UnsafeBurnerWalletAdapter } from "@solana/wallet-adapter-wallets";
 
 const SolanaProvider: React.FC<PropsWithChildren> = ({ children }) => {
   const network = WalletAdapterNetwork.Devnet;
-  const wallets = useMemo(() => [], []);
+  registerMoonGateWallet({
+    authMode: "Ethereum",
+    position: "top-left",
+  });
+  registerMoonGateWallet({
+    authMode: "Google",
+    position: "top-right",
+  });
+  registerMoonGateWallet({
+    authMode: "Twitter",
+    position: "top-right",
+  });
+  registerMoonGateWallet({
+    authMode: "Apple",
+    position: "top-right",
+  });
+  const wallets = useMemo(() => [new UnsafeBurnerWalletAdapter()], [network]);
   const endpoint = useMemo(() => clusterApiUrl(network), [network]);
   return (
     <ConnectionProvider endpoint={endpoint}>
